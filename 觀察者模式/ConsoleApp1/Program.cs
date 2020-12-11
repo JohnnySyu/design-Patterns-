9882d6efa0d2523d8e6557da747307f93bc60672
JohnnySyu <f87199443@gmail.com>
2020-12-11 22:51:47 +0800

    add 觀察者模式

---
+++ 觀察者模式/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Secretary
+        {
+            private IList<Observer> observers = new List<Observer>();
+            private string action;
+            public void Attach(Observer observer)
+            {
+                observers.Add(observer);
+            }
+
+            public void Detch(Observer observer)
+            {
+                observers.Remove(observer);
+            }
+
+            public void Notify()
+            {
+                foreach(stockObserver o in observers)
+                {
+                    o.Update();
+                }
+            }
+
+            public string SecretaryAction
+            {
+                get { return action; }
+                set { action = value; }
+            }
+        }
+        abstract class Observer
+        {
+            protected string name;
+            protected Secretary sub;
+
+            public Observer(string name, Secretary sub)
+            {
+                this.name = name;
+                this.sub = sub;
+            }
+
+            public abstract void Update();
+        }
+
+        class stockObserver : Observer
+        {
+            public stockObserver(string name, Secretary sub) : base(name, sub) { }
+            public override void Update()
+            {
+                Console.WriteLine("{0} {1} 關閉股票繼續工作", sub.SecretaryAction, name);
+
+            }
+
+        }
+
+        class NBAserver : Observer
+        {
+            public NBAserver(string name, Secretary sub) : base(name, sub)
+            {
+
+            }
+
+            public override void Update()
+            {
+                Console.WriteLine("{0} {1} 關閉NBA繼續工作", sub.SecretaryAction, name);
+            }
+        }
+
+      
+        static void Main(string[] args)
+        {
+            Secretary Marry = new Secretary();
+
+            stockObserver Marry1 = new stockObserver("瑪莉", Marry);
+            stockObserver Marry2 = new stockObserver("強尼", Marry);
+
+            Marry.Attach(Marry1);
+            Marry.Attach(Marry2);
+
+            Marry.SecretaryAction = "老闆回來了";
+
+            Marry.Notify();
+
+            Console.Read();
+        }
+    }
+
+   
+}
---
+++ 觀察者模式/解耦合/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 解耦合
+{
+    
+        class Program
+        {
+
+            interface Subject
+            {
+                void Attach(Observer observer);
+                void Detach(Observer observer);
+                void Notify();
+                string SubjectState
+                {
+                    get;
+                    set;
+                }
+            }
+
+            class Boss : Subject
+            {
+                private IList<Observer> observers = new List<Observer>();
+                private string action;
+                public void Attach(Observer observer)
+                {
+                    observers.Add(observer);
+                }
+
+                public void Detach(Observer observer)
+                {
+                    observers.Remove(observer);
+                }
+
+                public void Notify()
+                {
+                    foreach (Observer o in observers)
+                    {
+                        o.Update();
+                    }
+                }
+
+         
+
+            public string SubjectState 
+            {
+                get { return action; }
+                set { action = value; }
+            }
+        }
+            class Secretary : Subject
+            {
+                private IList<Observer> observers = new List<Observer>();
+                private string action;
+                public void Attach(Observer observer)
+                {
+                    observers.Add(observer);
+                }
+
+                public void Detach(Observer observer)
+                {
+                    observers.Remove(observer);
+                }
+
+                public void Notify()
+                {
+                    foreach (stockObserver o in observers)
+                    {
+                        o.Update();
+                    }
+                }
+
+            public string SubjectState
+            {
+                get { return action; }
+                set { action = value; }
+            }
+        }
+            abstract class Observer
+            {
+                protected string name;
+                protected Subject sub;
+
+                public Observer(string name, Subject sub)
+                {
+                    this.name = name;
+                    this.sub = sub;
+                }
+
+                public abstract void Update();
+            }
+
+            class stockObserver : Observer
+            {
+                public stockObserver(string name, Subject sub) : base(name, sub) { }
+                public override void Update()
+                {
+                    Console.WriteLine("{0} {1} 關閉股票繼續工作", sub.SubjectState, name);
+
+                }
+
+            }
+
+            class NBAObserver : Observer
+            {
+                public NBAObserver(string name, Subject sub) : base(name, sub)
+                {
+
+                }
+
+                public override void Update()
+                {
+                    Console.WriteLine("{0} {1} 關閉NBA繼續工作", sub.SubjectState, name);
+                }
+            }
+
+
+            static void Main(string[] args)
+            {
+               
+                 Boss boss = new Boss();
+
+                stockObserver Marry1 = new stockObserver("瑪莉", boss);
+                NBAObserver Marry2 = new NBAObserver("強尼", boss);
+
+                boss.Attach(Marry1);
+                boss.Attach(Marry2);
+
+                boss.Detach(Marry1);
+                boss.SubjectState=("老闆我回來啦");
+
+                boss.Notify();
+
+                Console.Read();
+            }
+        }
+    
+}
